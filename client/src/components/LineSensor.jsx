2ebaa6d5dadd8f276a276381f31ba2e943d36332
Relise228 <andreysmit43@gmail.com>
2020-07-21 13:40:22 +0300

    IntervalFix

--- client/src/components/LineSensor.jsx
+++ client/src/components/LineSensor.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 const LineSensor = (props) => {
+  console.log();
+
   return (
     <div className='line_sensor'>
       <div>{props.id}</div>
       <div>{props.model}</div>
       <div>{props.manufacture}</div>
-      <div>{new Date(props.interval).toLocaleTimeString().slice(0, -3)}</div>
+      <div>
+        {new Date(props.interval).toUTCString().slice(0, -3).split(' ')[4]}
+      </div>
       <div>{props.accuracy}</div>
       <div>{props.parametr}</div>
       <div>{props.unit}</div>
